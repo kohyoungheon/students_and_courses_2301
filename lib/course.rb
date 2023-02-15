49b25e68ab89420419566ef271400a056e4ac0d1
Young Koh <youngheon6@gmail.com>
2023-02-15 10:00:50 -0700

    feat: Define and test #enroll for Course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,9 +1,24 @@
 class Course
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :students
 
   def initialize(name,capacity)
     @name = name
     @capacity = capacity
     @students = []
+  end
+
+  def full?
+    if @students.length == @capacity
+      return true
+    end
+    false
+  end
+
+  def enroll(student)
+    if self.full? == false
+      @students << student
+    else
+      return "This class has #{@students.length}/#{@capacity} students"
+    end
   end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -8,6 +8,35 @@ describe Course do
       expect(course).to be_instance_of(Course)
       expect(course.name).to eq("Calculus")
       expect(course.capacity).to eq(2)
+      expect(course.students).to eq([])
+    end
+  end
+
+  describe '#full?' do
+    it "returns a boolean whether the course is at capacity." do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      expect(course.students).to eq([])
+      expect(course.full?).to eq(false)
+      course.enroll(student1)
+      course.enroll(student2)
+      expect(course.full?).to eq(true)
+    end
+  end
+
+  describe "#enroll" do
+    it "enrolls a student in a course" do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Mike", age: 25})
+
+      expect(course.students).to eq([])
+      course.enroll(student1)
+      course.enroll(student2)
+      expect(course.students).to eq([student1, student2])
+      expect(course.enroll(student3)).to eq("This class has 2/2 students")
     end
   end
 end
